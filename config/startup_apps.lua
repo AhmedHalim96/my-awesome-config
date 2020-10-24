b5cecad132ae70cda3e64e68e5786860702ad0a0
Ahmed Halim <ahmedhalim96@protonmail.com>
2020-10-24 10:29:19 +0200

    add modifications to startup apps

--- config/startup_apps.lua
+++ config/startup_apps.lua
@@ -7,20 +7,17 @@ local startup_apps = {
   '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
   'xfce4-power-manager', -- Power manager
    'flameshot',
-  --  'synology-drive -minimized',
-  --  'steam -silent',
     'touchpad-indicator',
+    'usr/bin/libinput-gestures',
     'copyq',
-  --  '/usr/bin/barrier',
+    'xiccd', -- Applies color profile
    'nitrogen --restore',
    'qbittorrent',
    'mintupdate-launcher',
    'xdman',
-  --  'guake',
-  --  '~/.local/bin/wallpaper', -- wallpaper-reddit script
   -- Add applications that need to be killed between reloads
   -- to avoid multipled instances, inside the awspawn script
-  '~/.config/awesome/config/awspawn' -- Spawn "dirty" apps that can linger between sessions
+  '~/.config/awesome/modules/awspawn' -- Spawn "dirty" apps that can linger between sessions
 
 }
 
