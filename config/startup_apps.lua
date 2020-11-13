2ac3325aaefcba19826102e12b6ab32ecb90c206
Ahmed Halim <ahmedhalim96@protonmail.com>
2020-11-13 10:28:28 +0200

    Adding some apps to startup

--- config/startup_apps.lua
+++ config/startup_apps.lua
@@ -1,6 +1,8 @@
 local startup_apps = {
-  "picom --config '/home/ahmed/.config/awesome/config/picom.conf'",
+  "picom --config"..string.format("%s/.config/awesome/config/picom.conf", os.getenv("HOME")),
   "setxkbmap -layout 'us,ara' -option 'grp:alt_shift_toggle'",
+  "setxkbmap -option terminate:ctrl_alt_bksp", --restart Xorg
+  "mintinstall-update-flatpak", -- Apply flatpak updates on startup
   'nm-applet --indicator', -- wifi
   'pnmixer', -- shows an audiocontrol applet in systray when installed.
   'blueberry-tray', -- Bluetooth tray icon
@@ -9,15 +11,18 @@ local startup_apps = {
   'xfce4-power-manager', -- Power manager
    'flameshot',
    "albert",
+   "/usr/bin/caffeine",
     'touchpad-indicator',
-    'usr/bin/libinput-gestures',
+    '/usr/bin/libinput-gestures',
+    'mpd',
     'copyq',
     'xiccd', -- Applies color profile
    'nitrogen --restore',
    'qbittorrent',
-   'cryptomator',
+   'cryptomator %F',
    'mintupdate-launcher',
-   'xdman',
+   '"/opt/xdman/jre/bin/java" -Xmx1024m -jar "/opt/xdman/xdman.jar" -m', --Xtereme Download Manager
+
   -- Add applications that need to be killed between reloads
   -- to avoid multipled instances, inside the awspawn script
   '~/.config/awesome/modules/awspawn' -- Spawn "dirty" apps that can linger between sessions
