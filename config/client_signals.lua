6cf7811e7940306ba4c358bfb12c4b11c2d9fb1b
Ahmed Halim <ahmedhalim96@protonmail.com>
2020-11-16 17:37:37 +0200

    fixing small mistake

--- config/client_signals.lua
+++ config/client_signals.lua
@@ -26,11 +26,11 @@ _G.client.connect_signal("request::titlebars", function(c)
   local buttons = gears.table.join(
       awful.button({ }, 1, function()
           c:emit_signal("request::activate", "titlebar", {raise = true})
-          awful.mouse._G.client.move(c)
+          awful.mouse.client.move(c)
       end),
       awful.button({ }, 3, function()
           c:emit_signal("request::activate", "titlebar", {raise = true})
-          awful.mouse._G.client.resize(c)
+          awful.mouse.client.resize(c)
       end)
   )
 
