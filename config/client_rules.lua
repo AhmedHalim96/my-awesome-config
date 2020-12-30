772fdc57c7d908eeffc8e4b8fab3f87ed975603b
Ahmed Halim <ahmedhalim96@protonmail.com>
2020-12-30 23:58:50 +0200

    making Wine floating

--- config/client_rules.lua
+++ config/client_rules.lua
@@ -42,6 +42,7 @@ awful.rules.rules = {
 				"Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
 				"Wpa_gui",
 				"veromix",
+				"Wine",
 				"xtightvncviewer"},
 
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
