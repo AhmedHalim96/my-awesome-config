517e2a39dd0cf8ce0ba1db47bba53080146ba29e
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-01-02 04:24:11 +0200

    centring volume icon

--- widgets/volume-control/volume-icon.lua
+++ widgets/volume-control/volume-icon.lua
@@ -5,13 +5,16 @@ local icon = "audio-volume-high-symbolic.svg"
 
   local volume_icon = wibox.widget {
     {
+      {
         image = base_path..icon,
         widget = wibox.widget.imagebox,
+      },
+      valign = 'center',
+      margins = 2,
+      widget = wibox.container.place,
     },
-    forced_height=20,
-    forced_width=20,
-    top=6,
-    left=2,
+    forced_height=16,
+    forced_width=16,
     right=2,
     widget = wibox.container.margin
   }
