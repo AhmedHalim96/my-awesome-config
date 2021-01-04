a0e467ac8612c4234127dee725f1a5e775b90ae9
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-01-04 21:15:45 +0200

    Fixing a small bug

--- rc.lua
+++ rc.lua
@@ -171,9 +171,10 @@ local myawesomemenu = {
   screen.connect_signal("arrange", function (s)
     local only_one = #s.tiled_clients == 1
     for _, c in pairs(s.clients) do
-        if only_one and not c.floating or c.maximized then
+        if ( only_one and (not c.floating or c.maximized)) or c.fullscreen then
             c.border_width = 0
-        else
+        
+        else 
             c.border_width = beautiful.border_width
         end
     end
