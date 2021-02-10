7a83460c70c15f2802db44fe2b67796be6e83d66
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-02-10 06:23:01 +0200

    updating colors

--- themes/multicolor/config/colors.lua
+++ themes/multicolor/config/colors.lua
@@ -2,18 +2,19 @@
 -- #Colors
 -- ###########################################################
 
-local colors        = {}
-colors.bg           = "#191c29"
-colors.bg_light     = '#262a3e'
-colors.titlebar_bg  = "#13151e"
-colors.primary      = "#c50ed2"
-colors.secondary    = "#00f3a1"
-colors.danger       = "#f50057"
-colors.white        = "#ffffff"
-colors.neon         = {}
-colors.neon.blue    = "#00e8c5"
-colors.neon.fuchsia = "#FE53BB"
-colors.neon.orange  = "#FF8211"
+local colors         = {}
+colors.bg            = "#191c29"
+colors.bg_light      = '#262a3e'
+colors.titlebar_bg   = "#13151e"
+colors.primary       = "#c50ed2"
+colors.secondary     = "#00f3a1"
+colors.secondary_dim = "#1b5d5b"
+colors.danger        = "#f50057"
+colors.white         = "#ffffff"
+colors.neon          = {}
+colors.neon.blue     = "#00e8c5"
+colors.neon.fuchsia  = "#FE53BB"
+colors.neon.orange   = "#FF8211"
 
 
 return colors
