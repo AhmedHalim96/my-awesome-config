274900945caaade373d3f631cc713c0dfa691de7
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-03-02 04:37:49 +0200

    merging taglist_buttons with taglist

--- themes/neon/widgets/taglist.lua
+++ themes/neon/widgets/taglist.lua
@@ -1,7 +1,28 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 local dpi   = require("beautiful.xresources").apply_dpi
+local gears = require("gears")
 local wibox = require("wibox")
+
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+-- taglist buttons
+awful.util.taglist_buttons = my_table.join(
+    awful.button({ }, 1, function(t) t:view_only() end),
+    awful.button({ modkey }, 1, function(t)
+        if client.focus then
+            client.focus:move_to_tag(t)
+        end
+    end),
+    awful.button({ }, 3, awful.tag.viewtoggle),
+    awful.button({ modkey }, 3, function(t)
+        if client.focus then
+            client.focus:toggle_tag(t)
+        end
+    end),
+    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+)
 
 local function taglist(s)
   s.taglist = awful.widget.taglist {
