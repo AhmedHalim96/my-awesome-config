4479b2b91a1aa58963851d02a82eea46f25241be
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-03-02 03:44:56 +0200

    extracting the taglist widget

--- themes/neon/theme.lua
+++ themes/neon/theme.lua
@@ -265,53 +265,8 @@ function theme.at_screen_connect(s)
     
 
     -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
-
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = awful.util.taglist_buttons,
-
-        layout   = {
-            layout  = wibox.layout.fixed.horizontal
-        },
-        widget_template = {
-            {
-                {          
-                    {
-                        id     = 'text_role',
-                        widget = wibox.widget.textbox,
-                    },
-                    layout = wibox.layout.fixed.horizontal,
-                },
-                left  = 10,
-                right = 10,              
-                widget = wibox.container.margin
-            },
-            
-        -- Add support for hover colors
-        create_callback = function(self, c, index, objects)
-            self:connect_signal('mouse::enter', function()
-                self.bg = theme.bg_focus
-            end)
-            self:connect_signal('mouse::leave', function()
-                local selected_tag = mouse.screen.selected_tag
-               if selected_tag then
-                    if mouse.screen.selected_tag.index ~= index then
-                        self.bg = theme.bg
-                    end
-               else  self.bg = theme.bg
-               end
-            end)
-        end,
-    
-            id     = 'background_role',
-            widget = wibox.container.background,
-            
-        },
-    
-
-    }
+    local _taglist = require("themes.neon.widgets.taglist")(s)
+    s.mytaglist = _taglist
 
 
     -- Create a tasklist widget
---
+++ themes/neon/widgets/taglist.lua
@@ -0,0 +1,54 @@
+local awful = require("awful")
+local beautiful = require("beautiful")
+local dpi   = require("beautiful.xresources").apply_dpi
+local wibox = require("wibox")
+
+local function taglist(s)
+  return awful.widget.taglist {
+    screen  = s,
+    filter  = awful.widget.taglist.filter.all,
+    buttons = awful.util.taglist_buttons,
+
+    layout   = {
+        layout  = wibox.layout.fixed.horizontal
+    },
+    widget_template = {
+        {
+            {          
+                {
+                    id     = 'text_role',
+                    widget = wibox.widget.textbox,
+                },
+                layout = wibox.layout.fixed.horizontal,
+            },
+            left  = dpi(10),
+            right = dpi(10),              
+            widget = wibox.container.margin
+        },
+        
+    -- Add support for hover colors
+    create_callback = function(self, c, index, objects)
+        self:connect_signal('mouse::enter', function()
+            self.bg = beautiful.bg_focus
+        end)
+        self:connect_signal('mouse::leave', function()
+            local selected_tag = mouse.screen.selected_tag
+           if selected_tag then
+                if mouse.screen.selected_tag.index ~= index then
+                    self.bg = beautiful.bg
+                end
+           else  self.bg = beautiful.bg
+           end
+        end)
+    end,
+
+        id     = 'background_role',
+        widget = wibox.container.background,
+        
+    },
+
+
+}
+end
+
+return taglist
