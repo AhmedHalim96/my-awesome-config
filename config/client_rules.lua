254881ef4ddf8f0623eed578c003472b13a51c26
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-05-08 15:39:27 +0200

    adding peertube rule

--- config/client_rules.lua
+++ config/client_rules.lua
@@ -200,8 +200,16 @@ awful.rules.rules = {
 		properties = { screen = 1, tag = "2" } },
 		
 	-- clemintine
-	{ rule = { class = "Clementine" },
-	  properties = { screen = 1, tag = "5" } },
+	{ 
+		rule = { class = "Clementine" },
+		properties = { screen = 1, tag = "5" } 
+	},
+
+	-- peertube 
+	{ 
+		rule = { class = "FreeTube" },
+		properties = { screen = 1, tag = "7" } 
+	},
 	
 	-- Set Firefox to always map on the tag named "2" on screen 1.
 	-- { rule = { class = "Firefox" },
