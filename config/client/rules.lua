e6688070a857f2e08513843bc367d58650947b39
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-09-13 16:25:18 +0200

    adding rules for wine

--- config/client/rules.lua
+++ config/client/rules.lua
@@ -266,6 +266,13 @@ awful.rules.rules = {
 		properties = { floating = false }
 	},
 
+  -- wine 
+	{ 
+		rule = { class = "Wine" },
+		properties = { screen = 1, tag = "9" }
+	},
+
+
 	-- Set Firefox to always map on the tag named "2" on screen 1.
 	-- { rule = { class = "Firefox" },
 	--   properties = { screen = 1, tag = "2" } },
