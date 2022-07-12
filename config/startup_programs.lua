137a2c53996232d3ba748cc1ac48fa433f9b215b
Ahmed Halim <ahmedhalim96@protonmail.com>
2022-07-12 10:40:02 +0200

    Adding bluman applet to startup programs + cleaning

--- config/startup_programs.lua
+++ config/startup_programs.lua
@@ -3,14 +3,12 @@ return {
     "picom-i",
     "xrdb -load ~/.config/X11/Xresources",
     "kb-startup-script",
-    --"mintinstall-update-flatpak", -- Apply flatpak updates on startup -- removed in mint 20.2
     'polkit', -- Credential manager
     "albert",
     "breaktimer",
     'touchpad-indicator',
     'nm-applet --indicator', 
-    -- 'pnmixer',
-    'blueberry-tray', 
+    'blueman-applet',
     'xfce4-power-manager',
     'flameshot',
     'copyq',
@@ -21,7 +19,6 @@ return {
     'appimagelauncherd',
     --Xtereme Download Manager
     '"/opt/xdman/jre/bin/java" -Xmx1024m -jar "/opt/xdman/xdman.jar" -m', 
-    -- 'cryptomator %F',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/modules/awspawn' -- Spawn "dirty" apps that can linger between sessions
